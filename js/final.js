80041775c592fc05679b139933d04c3a56716bc8
Raquelfgb <kelkinha@hotmail.com>
2023-05-30 08:53:59 -0600

    API

--- js/final.js
+++ js/final.js
@@ -119,23 +119,70 @@ function renderRecipe(storage){
 
 
 //API
-
-let search = document.getElementById('search');
-
-const axios = require('axios');
-
+const cards = document.querySelector('.cards');
+const meal = document.getElementById('meal');
+const details = document.querySelector('.detail');
+const close = document.getElementById('close');
+const url = 'https://low-carb-recipes.p.rapidapi.com/search?name=cake&tags=keto%3Bdairy-free&includeIngredients=egg%3Bbutter&excludeIngredients=cinnamon&maxPrepareTime=10&maxCookTime=20&maxCalories=500&maxNetCarbs=5&maxSugar=3&maxAddedSugar=0&limit=10';
 const options = {
-  method: 'GET',
-  url: 'https://low-carb-recipes.p.rapidapi.com/recipes/2807982c-986a-4def-9e3a-153a3066af7a',
-  headers: {
-    'X-RapidAPI-Key': 'd574b71761mshd6036c941960d4cp13806ejsn7f8ba0da29ce',
-    'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd574b71761mshd6036c941960d4cp13806ejsn7f8ba0da29ce',
+		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
+	}
 };
 
 try {
-	const response = await axios.request(options);
-	console.log(response.data);
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
 } catch (error) {
 	console.error(error);
+}
+
+
+function getMeal(recipe){
+	let card = document.createElement('section');
+	let name = document.createElement('h2');
+	let photo = document.createElement('img');
+    let tags = document.createElement('h3');
+    let description = document.createElement('p');
+    let prepareTime = document.createElement('h3');
+    let cookTime = document.createElement('h3');
+	let ingredients = document.createElement('h3');
+	let steps = document.createElement('h3');
+    let servings = document.createElement('h3');
+    let servingSize = document.createElement('h3');
+    let nutrients = document.createElement('h3');
+    
+
+    name.textContent = (`${recipe.name}`);
+    description.textContent = (`${recipe.description}`);
+    tags.textContent = (`${recipe.tag}`);
+	prepareTime.textContent = (`${recipe.prepareTime}`);
+    cookTime.textContent = (`${recipe.cookTime}`);
+	ingredients.textContent = (`${recipe.ingredients}`);
+    steps.textContent = (`${recipe.steps}`);
+	servings.textContent = (`${recipe.servings}`);
+    servingSize.textContent = (`${recipe.servingSize}`);
+	nutrients.textContent = (`${recipe.nutrients}`);
+    
+
+    photo.setAttribute('src', recipe.image);
+    photo.setAttribute('alt', (`Recipe of ${recipe.name}`));
+    photo.setAttribute('loading', 'lazy');
+
+    card.appendChild(name);
+    card.appendChild(photo);
+    card.appendChild(prepareTime);
+    card.appendChild(cookTime);
+	card.appendChild(tags);
+    card.appendChild(ingredients);
+    card.appendChild(description);
+	card.appendChild(steps);
+    card.appendChild(servings);
+    card.appendChild(servingSize);
+	card.appendChild(nutrients);
+
+    document.querySelector('div.cards').appendChild(card);
 }
